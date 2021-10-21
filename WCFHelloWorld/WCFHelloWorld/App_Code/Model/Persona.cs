f7f996887c3ca571c15a73f1152cbe45e7e2d0c7
Elia Capiluppi <eliacapiluppi02gmail.com>
2021-10-21 13:49:49 +0200

    aa

---
+++ WCFHelloWorld/WCFHelloWorld/App_Code/Model/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Persona
+/// </summary>
+[DataContract]
+public class Persona
+{
+    [DataMember]
+    public string Nome { get; set; }
+    [DataMember]
+    public string Cognome { get; set; }
+    [DataMember]
+    public DateTime DataNascita { get; set; }
+
+    public Persona(string cognome,string nome,DateTime dataNascita)
+    {
+        this.Cognome = cognome;
+        this.Nome = nome;
+        this.DataNascita = dataNascita;
+    }
+}
